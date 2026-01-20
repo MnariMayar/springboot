90fd9b2b8cf211d4c554b2731c52604559483d0e
mayar <mayar.mnari@esprit.tn>
2026-01-20 11:07:10 +0100

    first project

---
+++ mayar-mnari-arctic3/src/main/java/tn/esprit/mayarmnariarctic3/MayarMnariArctic3Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.mayarmnariarctic3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayarMnariArctic3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MayarMnariArctic3Application.class, args);
+    }
+
+}
